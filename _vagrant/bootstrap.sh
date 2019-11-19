0eadb20f1e5259ad29a4ebd5a7c147bd610c9e49
Shahnewaz Rifat <rifat.shahnewaz@crossover.com>
2019-11-19 18:20:40 +0600

    make it simple
    - make it simple

--- _vagrant/bootstrap.sh
+++ _vagrant/bootstrap.sh
@@ -5,22 +5,13 @@ PASSWORD='12345678'
 PROJECTFOLDER='myproject'
 
 sudo apt-get update
-sudo apt-get -y upgrade
 
 sudo apt-get install -y apache2
-sudo apt-get install -y php5
 
 sudo debconf-set-selections <<< "mysql-server mysql-server/root_password password $PASSWORD"
 sudo debconf-set-selections <<< "mysql-server mysql-server/root_password_again password $PASSWORD"
 sudo apt-get -y install mysql-server
 sudo apt-get install php5-mysql
-
-sudo debconf-set-selections <<< "phpmyadmin phpmyadmin/dbconfig-install boolean true"
-sudo debconf-set-selections <<< "phpmyadmin phpmyadmin/app-password-confirm password $PASSWORD"
-sudo debconf-set-selections <<< "phpmyadmin phpmyadmin/mysql/admin-pass password $PASSWORD"
-sudo debconf-set-selections <<< "phpmyadmin phpmyadmin/mysql/app-pass password $PASSWORD"
-sudo debconf-set-selections <<< "phpmyadmin phpmyadmin/reconfigure-webserver multiselect apache2"
-sudo apt-get -y install phpmyadmin
 
 # Create project folder, written in 3 single mkdir-statements to make sure this runs everywhere without problems
 sudo mkdir "/var/www"
@@ -49,19 +40,8 @@ service apache2 restart
 # remove default apache index.html
 sudo rm "/var/www/html/index.html"
 
-# install git
-sudo apt-get -y install git
 
-# git clone MINI
-sudo git clone https://github.com/panique/mini "/var/www/html/${PROJECTFOLDER}"
-
-# install Composer (not necessary by default)
-#curl -s https://getcomposer.org/installer | php
-#mv composer.phar /usr/local/bin/composer
-
-# go to project folder, load Composer packages (not necessary by default)
-#cd "/var/www/html/${PROJECTFOLDER}"
-#composer install --dev
+sudo cp -R ~/mini/* "/var/www/html/${PROJECTFOLDER}/"
 
 # run SQL statements from MINI folder
 sudo mysql -h "localhost" -u "root" "-p${PASSWORD}" < "/var/www/html/${PROJECTFOLDER}/_install/01-create-database.sql"
