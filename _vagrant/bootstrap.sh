1a8d6a0b122ac496fe3fca1c83a7497ec9651e23
Shahnewaz Rifat <rifat.shahnewaz@crossover.com>
2019-12-27 20:03:30 +0600

    remove sudo
    - remove sudo

--- _vagrant/bootstrap.sh
+++ _vagrant/bootstrap.sh
@@ -4,19 +4,19 @@
 PASSWORD='12345678'
 PROJECTFOLDER='myproject'
 
-sudo apt-get update
+apt-get update
 
-sudo apt-get install -y apache2
+apt-get install -y apache2
 
-sudo debconf-set-selections <<< "mysql-server mysql-server/root_password password $PASSWORD"
-sudo debconf-set-selections <<< "mysql-server mysql-server/root_password_again password $PASSWORD"
-sudo apt-get -y install mysql-server
-sudo apt-get install php5-mysql
+debconf-set-selections <<< "mysql-server mysql-server/root_password password $PASSWORD"
+debconf-set-selections <<< "mysql-server mysql-server/root_password_again password $PASSWORD"
+apt-get -y install mysql-server
+apt-get install php5-mysql
 
 # Create project folder, written in 3 single mkdir-statements to make sure this runs everywhere without problems
-sudo mkdir "/var/www"
-sudo mkdir "/var/www/html"
-sudo mkdir "/var/www/html/${PROJECTFOLDER}"
+mkdir "/var/www"
+mkdir "/var/www/html"
+mkdir "/var/www/html/${PROJECTFOLDER}"
 
 # setup hosts file
 VHOST=$(cat <<EOF
@@ -33,25 +33,25 @@ EOF
 echo "${VHOST}" > /etc/apache2/sites-available/000-default.conf
 
 # enable mod_rewrite
-sudo a2enmod rewrite
+a2enmod rewrite
 
 # restart apache
 service apache2 restart
 service mysql restart
 
 # remove default apache index.html
-sudo rm "/var/www/html/index.html"
+rm "/var/www/html/index.html"
 
 
-sudo cp -R * "/var/www/html/${PROJECTFOLDER}/"
+cp -R * "/var/www/html/${PROJECTFOLDER}/"
 
 # run SQL statements from MINI folder
-sudo mysql -h "localhost" -u "root" "-p${PASSWORD}" < "/var/www/html/${PROJECTFOLDER}/_install/01-create-database.sql"
-sudo mysql -h "localhost" -u "root" "-p${PASSWORD}" < "/var/www/html/${PROJECTFOLDER}/_install/02-create-table-song.sql"
-sudo mysql -h "localhost" -u "root" "-p${PASSWORD}" < "/var/www/html/${PROJECTFOLDER}/_install/03-insert-demo-data-into-table-song.sql"
+mysql -h "localhost" -u "root" "-p${PASSWORD}" < "/var/www/html/${PROJECTFOLDER}/_install/01-create-database.sql"
+mysql -h "localhost" -u "root" "-p${PASSWORD}" < "/var/www/html/${PROJECTFOLDER}/_install/02-create-table-song.sql"
+mysql -h "localhost" -u "root" "-p${PASSWORD}" < "/var/www/html/${PROJECTFOLDER}/_install/03-insert-demo-data-into-table-song.sql"
 
 # put the password into the application's config. This is quite hardcore, but why not :)
-sudo sed -i "s/your_password/${PASSWORD}/" "/var/www/html/${PROJECTFOLDER}/application/config/config.php"
+sed -i "s/your_password/${PASSWORD}/" "/var/www/html/${PROJECTFOLDER}/application/config/config.php"
 
 # final feedback
 echo "Voila!"
